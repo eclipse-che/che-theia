4a9d1b15309533c23eedde7bd83f7f7a96e0583a
Florent BENOIT <fbenoit@redhat.com>
2019-02-01 09:42:51 +0100

    Add welcome page plug-in (#40)
    * Add welcome page plug-in

---
+++ plugins/welcome-plugin/resources/welcome-page.js
@@ -0,0 +1,25 @@
+/*********************************************************************
+ * Copyright (c) 2019 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+
+let api;
+
+(function () {
+    if (typeof acquireTheiaApi === 'undefined') {
+        return
+    }
+    api = acquireTheiaApi();
+}());
+
+function executeCommand(commandName) {
+    api.postMessage({
+        command: 'executeCommand',
+        commandName: commandName
+    });
+}
