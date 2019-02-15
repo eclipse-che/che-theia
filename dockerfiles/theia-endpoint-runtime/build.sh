948d6d88cbdbf5ed493e7949392ef1e6af487c7b
Anatoliy Bazko <abazko@redhat.com>
2019-02-15 08:46:43 +0200

    Move che-theia-endpoint-runtime into dockerfiles (#64)
    * Introduce theia-endpoint-runtime
    
    Signed-off-by: Anatoliy Bazko <abazko@redhat.com>

---
+++ dockerfiles/theia-endpoint-runtime/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright (c) 2019 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+
+base_dir=$(cd "$(dirname "$0")"; pwd)
+. "${base_dir}"/../build.include
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+LOCAL_ASSEMBLY_DIR="${DIR}"/theia-plugin-ext
+
+if [ -d "${LOCAL_ASSEMBLY_DIR}" ]; then
+  rm -r "${LOCAL_ASSEMBLY_DIR}"
+fi
+
+#in mac os 'cp' cannot create destination dir, so create it first
+mkdir ${LOCAL_ASSEMBLY_DIR}
+
+echo "Copying ${base_dir}/../../extensions/eclipse-che-theia-plugin-ext --> ${LOCAL_ASSEMBLY_DIR}"
+cp -r "${base_dir}/../../extensions/eclipse-che-theia-plugin-ext/." "${LOCAL_ASSEMBLY_DIR}"
+
+
+init --name:theia-endpoint-runtime "$@"
+build
