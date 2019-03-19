52425a2a36a527d7670ea5bcd63dbe7456dd60f7
Yevhen Vydolob <yvydolob@redhat.com>
2019-03-19 16:15:19 +0200

    eclipse/che#12306 move che-theia-hosted-plugin-manager-extension
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>

---
+++ extensions/che-theia-hosted-plugin-manager-extension/src/node/che-workspace-hosted-plugin-uri-post-processor.ts
@@ -0,0 +1,79 @@
+/*********************************************************************
+ * Copyright (c) 2018 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+
+import { injectable } from 'inversify';
+import URI from '@theia/core/lib/common/uri';
+import { HostedPluginUriPostProcessor } from '@theia/plugin-ext';
+import WorkspaceClient, { IRemoteAPI, IRestAPIConfig } from '@eclipse-che/workspace-client';
+import { che } from '@eclipse-che/api';
+
+@injectable()
+export class CheWorkspaceHostedPluginUriPostProcessor implements HostedPluginUriPostProcessor {
+    protected restApiClient: IRemoteAPI;
+
+    constructor() {
+        const restAPIConfig: IRestAPIConfig = {};
+        restAPIConfig.baseUrl = process.env.CHE_API;
+        const token = process.env.CHE_MACHINE_TOKEN;
+        if (token) {
+            restAPIConfig.headers = {};
+            restAPIConfig.headers['Authorization'] = token;
+        }
+        this.restApiClient = WorkspaceClient.getRestApi(restAPIConfig);
+    }
+
+    async processUri(uri: URI): Promise<URI> {
+        const hostedPluginTheiaInstanceServer = await this.getHostedPluginTheiaInstanceServer();
+        if (!hostedPluginTheiaInstanceServer) {
+            throw new Error('No server with type "ide-dev" found.');
+        }
+
+        const externalUri = new URI(hostedPluginTheiaInstanceServer.url);
+        return externalUri;
+    }
+
+    /**
+     * Searches for server which exposes hosted Theia instance.
+     * The server label is the attribute "type": "ide-dev".
+     */
+    protected async getHostedPluginTheiaInstanceServer(): Promise<che.workspace.Server | undefined> {
+        const workspace = await this.getCurrentWorkspace();
+        if (!workspace.runtime) {
+            throw new Error('Workspace is not running.');
+        }
+
+        const machines = workspace.runtime.machines!;
+        for (const machineName in machines) {
+            if (!machines.hasOwnProperty(machineName)) {
+                continue;
+            }
+            const servers = machines[machineName].servers!;
+            for (const serverName in servers) {
+                if (!servers.hasOwnProperty(serverName)) {
+                    continue;
+                }
+                const serverAttributes = servers[serverName].attributes;
+                if (serverAttributes && serverAttributes['type'] === 'ide-dev') {
+                    return servers[serverName];
+                }
+            }
+        }
+        return undefined;
+    }
+
+    protected async getCurrentWorkspace(): Promise<che.workspace.Workspace> {
+        const workspaceId = process.env.CHE_WORKSPACE_ID;
+        if (!workspaceId) {
+            throw new Error('Environment variable CHE_WORKSPACE_ID is not set.');
+        }
+        return await this.restApiClient.getById<che.workspace.Workspace>(workspaceId);
+    }
+
+}
---
+++ extensions/che-theia-hosted-plugin-manager-extension/src/node/hosted-plugin-manager-contribution-extension-backend-module.ts
@@ -0,0 +1,17 @@
+/*********************************************************************
+ * Copyright (c) 2018 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+
+import { ContainerModule } from 'inversify';
+import { HostedPluginUriPostProcessorSymbolName } from '@theia/plugin-ext';
+import { CheWorkspaceHostedPluginUriPostProcessor } from './che-workspace-hosted-plugin-uri-post-processor';
+
+export default new ContainerModule(bind => {
+    bind(Symbol.for(HostedPluginUriPostProcessorSymbolName)).to(CheWorkspaceHostedPluginUriPostProcessor);
+});
