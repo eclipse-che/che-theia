13cdf6f19909a9beb2f2f3809c70cf62de48b094
Artem Zatsarynnyi <azatsary@redhat.com>
2019-05-16 17:44:27 +0300

    Allow Kubernetes Tooling Che Plugin use Buildah as an image builder instead of Docker daemon (#220)
    * Allow Kubernetes Tooling Che Plugin use Buildah as an image builder instead of Docker daemon
    
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>
    
    * Allow Kubernetes Tooling Che Plugin use Buildah as an image builder instead of Docker daemon
    
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>
    
    * fixes after Florent's review
    
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>
    
    * fix after Florent's review
    
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

---
+++ dockerfiles/remote-plugin-kubernetes-tooling-1.0.0/etc/docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2019 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+set -e
+
+CMD=$1
+if [[ $1 = build ]]; then
+    CMD="bud"
+fi
+shift
+buildah $CMD $@
