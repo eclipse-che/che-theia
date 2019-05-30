b5a1fe2d30dc7f2051bd8373afdd703cc5080fdd
Anatoliy Bazko <abazko@redhat.com>
2019-05-30 17:53:59 +0300

    Add Eclipse Che Theia plugin remote extension
    Signed-off-by: Anatoliy Bazko <abazko@redhat.com>

---
+++ extensions/eclipse-che-theia-plugin-remote/src/node/server-plugin-proxy-runner.ts
@@ -0,0 +1,58 @@
+/*********************************************************************
+ * Copyright (c) 2018-2019 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+
+import { injectable, inject } from 'inversify';
+import { HostedPluginClient, ServerPluginRunner, PluginMetadata } from '@theia/plugin-ext/lib/common/plugin-protocol';
+import { HostedPluginRemote } from './hosted-plugin-remote';
+
+/**
+ * Proxy runner being a facade for loading plugins locally or remotely
+ */
+@injectable()
+export class ServerPluginProxyRunner implements ServerPluginRunner {
+
+    @inject(HostedPluginRemote)
+    protected readonly hostedPluginRemote: HostedPluginRemote;
+
+    private defaultRunner: ServerPluginRunner;
+
+    public setDefault(defaultRunner: ServerPluginRunner): void {
+        this.defaultRunner = defaultRunner;
+    }
+
+    public setClient(client: HostedPluginClient): void {
+        this.hostedPluginRemote.setClient(client);
+    }
+
+    public clientClosed(): void {
+        this.hostedPluginRemote.clientClosed();
+    }
+
+    // tslint:disable-next-line:no-any
+    public acceptMessage(jsonMessage: any): boolean {
+        return jsonMessage.pluginID !== undefined;
+    }
+
+    // tslint:disable-next-line:no-any
+    public onMessage(jsonMessage: any): void {
+        // do routing on the message
+        if (this.hostedPluginRemote.hasEndpoint(jsonMessage.pluginID)) {
+            this.hostedPluginRemote.onMessage(jsonMessage);
+        } else {
+            this.defaultRunner.onMessage(jsonMessage.content);
+        }
+
+    }
+
+    getExtraPluginMetadata(): Promise<PluginMetadata[]> {
+        return this.hostedPluginRemote.getExtraPluginMetadata();
+    }
+
+}
---
+++ extensions/eclipse-che-theia-plugin-remote/src/node/terminal-container-aware.ts
@@ -0,0 +1,66 @@
+/*********************************************************************
+ * Copyright (c) 2018-2019 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+
+import * as theia from '@theia/plugin';
+import { TerminalServiceExtImpl } from '@theia/plugin-ext/lib/plugin/terminal-ext';
+import { DebugExtImpl } from '@theia/plugin-ext/lib/plugin/node/debug/debug';
+
+/**
+ * Allow to override createTerminal to be container-aware and then create terminal to the sidecar container
+ */
+export class TerminalContainerAware {
+
+    /**
+     * Intercept the original method by adding the CHE_MACHINE_NAME as attribute (if exists)
+     */
+    overrideTerminal(terminalServiceExt: TerminalServiceExtImpl) {
+        // bind createTerminal to the scope 'this' of the terminalServiceExt.
+        const originalCreateTerminal = terminalServiceExt.createTerminal.bind(terminalServiceExt);
+
+        const createTerminal = (nameOrOptions: theia.TerminalOptions | (string | undefined), shellPath?: string, shellArgs?: string[]) => {
+            let options: theia.TerminalOptions;
+            if (typeof nameOrOptions === 'object') {
+                options = nameOrOptions;
+            } else {
+                options = {
+                    name: nameOrOptions,
+                    shellPath: shellPath,
+                    shellArgs: shellArgs,
+                    attributes: {}
+                };
+            }
+
+            // add machine name if exists
+            if (process.env.CHE_MACHINE_NAME) {
+                if (!options.attributes) {
+                    options.attributes = {};
+                }
+                options.attributes['CHE_MACHINE_NAME'] = process.env.CHE_MACHINE_NAME;
+            }
+
+            return originalCreateTerminal(options, shellPath, shellArgs);
+        };
+
+        // override terminal
+        terminalServiceExt.createTerminal = createTerminal;
+    }
+
+    overrideTerminalCreationOptionForDebug(debugExt: DebugExtImpl) {
+        debugExt.doGetTerminalCreationOptions = (debugType: string) => {
+            const options: theia.TerminalOptions = {
+                attributes: {
+                    'CHE_MACHINE_NAME': process.env.CHE_MACHINE_NAME
+                }
+            };
+
+            return Promise.resolve(options);
+        };
+    }
+}
