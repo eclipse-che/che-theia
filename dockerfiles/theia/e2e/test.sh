f75c55e5d52ac3299cc10b28375bb5b1f10be46d
Masaki Muranaka <monaka@monami-ya.com>
2019-06-18 04:55:21 +0000

    Add e2e test for non root runtimes.
    Signed-off-by: Masaki Muranaka <monaka@monami-ya.com>

--- dockerfiles/theia/e2e/test.sh
+++ dockerfiles/theia/e2e/test.sh
@@ -20,10 +20,15 @@ fi
 # Runs E2E tests in a docker container.
 run_test_in_docker_container() {
   docker_exec run --rm ${DOCKER_RUN_OPTIONS} \
+       --user $1 \
        -v "${base_dir}/videos":/root/cypress/videos \
        -v "${base_dir}/logs":/root/logs \
        -v /var/run/docker.sock:/var/run/docker.sock \
            $IMAGE_NAME
 }
 
-run_test_in_docker_container
+# Run as root
+run_test_in_docker_container '0:0'
+
+# Run as non root
+run_test_in_docker_container '1234:5678'
