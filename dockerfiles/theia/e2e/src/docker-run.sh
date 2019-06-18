57821880a33a67fd8743f64978fec145ad910e48
Masaki Muranaka <monaka@monami-ya.com>
2019-06-18 01:42:03 +0000

    Watch if Theia starts listening the port.
    Signed-off-by: Masaki Muranaka <monaka@monami-ya.com>

--- dockerfiles/theia/e2e/src/docker-run.sh
+++ dockerfiles/theia/e2e/src/docker-run.sh
@@ -6,13 +6,32 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 
+# (5 + 2 * 30 = 65 seconds) is the default timeout.
+: "${WAIT_COUNT:=30}"
+
 echo "Starting Theia..."
 rm -rf /root/logs/*
 HOME=/home/theia /entrypoint.sh > /root/logs/theia.log 2>/root/logs/theia-error.log&
-sleep 10s
+
 echo "Cleaning videos folder..."
 # Cleanup previous videos
-rm -rf /root/cypress/videos
+rm -rf /root/cypress/videos/*
+
+# Find TCP 0.0.0.0:3100 that will be opened by Theia.
+sleep 5s
+while [ $WAIT_COUNT -gt 0 ]; do
+    # Check the listening port
+    ss -nlt | grep -Fq ':3100' && break
+    # not found
+    WAIT_COUNT=$((WAIT_COUNT-1));
+    echo "Waiting for booting up Theia..."
+    sleep 2s
+done
+
+if [ $WAIT_COUNT -eq 0 ]; then
+    echo "Timeout. Theia is dead?"
+    exit 1
+fi
 
 # Run tests
 echo "Run the tests"
