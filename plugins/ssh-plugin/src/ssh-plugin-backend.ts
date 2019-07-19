5a9da8d6ccf47afbb60c3f54179446e3d0bc0422
Igor Vinokur <ivinokur@redhat.com>
2019-07-19 15:07:01 +0300

    fixup! Store SSH private key on the file system
    Signed-off-by: Igor Vinokur <ivinokur@redhat.com>

--- plugins/ssh-plugin/src/ssh-plugin-backend.ts
+++ plugins/ssh-plugin/src/ssh-plugin-backend.ts
@@ -11,7 +11,8 @@
 import * as theia from '@theia/plugin';
 import { RemoteSshKeyManager, SshKeyManager } from './node/ssh-key-manager';
 import { che as cheApi } from '@eclipse-che/api';
-import * as fs from 'fs';
+import { resolve } from 'path';
+import { pathExists, unlink, ensureFile, chmod, readFile, writeFile, appendFile } from 'fs-extra';
 import * as os from 'os';
 
 export async function start() {
@@ -59,37 +60,36 @@ const getHostName = async function (): Promise<string> {
     return hostName ? hostName : '';
 };
 
-const updateConfig = function (hostName: string): void {
-    const sshDir = os.homedir() + '/.ssh';
-    const configFile = sshDir + '/config';
-    if (!fs.existsSync(configFile)) {
-        if (!fs.existsSync(sshDir)) {
-            fs.mkdirSync(sshDir);
-        }
-        fs.appendFileSync(configFile, '');
-        fs.chmodSync(configFile, '644');
-    }
-    const keyConfig = `\nHost ${hostName.startsWith('default-') ? '*' : hostName}\nIdentityFile ${sshDir}/${hostName.replace('.', '_')}\n`;
-    const configContent = fs.readFileSync(configFile).toString();
-    if (configContent.indexOf(keyConfig) >= 0) {
-        const newConfigContent = configContent.replace(keyConfig, '');
-        fs.writeFileSync(configFile, newConfigContent);
+const getKeyFilePath = function (name: string): string {
+    return resolve(os.homedir(), '.ssh', name.replace('.', '_'));
+};
+
+const updateConfig = async function (hostName: string): Promise<void> {
+    const sshDir = resolve(os.homedir(), '.ssh');
+    const configFile = resolve(sshDir, 'config');
+    await ensureFile(configFile);
+    await chmod(configFile, '644');
+    const keyConfig = `\nHost ${hostName.startsWith('default-') ? '*' : hostName}\nIdentityFile ${getKeyFilePath(hostName)}\n`;
+    const configContentBuffer = await readFile(configFile);
+    if (configContentBuffer.indexOf(keyConfig) >= 0) {
+        const newConfigContent = configContentBuffer.toString().replace(keyConfig, '');
+        await writeFile(configFile, newConfigContent);
     } else {
-        fs.appendFileSync(configFile, keyConfig);
+        await appendFile(configFile, keyConfig);
     }
 };
 
-const writeKey = function (name: string, key: string): void {
-    const keyFile = os.homedir() + '/.ssh/' + name.replace('.', '_');
-    fs.appendFileSync(keyFile, key);
-    fs.chmodSync(keyFile, '600');
+const writeKey = async function (name: string, key: string): Promise<void> {
+    const keyFile = getKeyFilePath(name);
+    await appendFile(keyFile, key);
+    await chmod(keyFile, '600');
 };
 
 const generateKeyPair = async function (sshkeyManager: SshKeyManager): Promise<void> {
     const keyName = `default-${Date.now()}`;
     const key = await sshkeyManager.generate('vcs', keyName);
-    updateConfig(keyName);
-    writeKey(keyName, key.privateKey);
+    await updateConfig(keyName);
+    await writeKey(keyName, key.privateKey);
     const viewAction = 'View';
     const action = await theia.window.showInformationMessage('Key pair successfully generated, do you want to view the public key?', viewAction);
     if (action === viewAction && key.privateKey) {
@@ -101,8 +101,8 @@ const generateKeyPair = async function (sshkeyManager: SshKeyManager): Promise<v
 const generateKeyPairForHost = async function (sshkeyManager: SshKeyManager): Promise<void> {
     const hostName = await getHostName();
     const key = await sshkeyManager.generate('vcs', hostName);
-    updateConfig(hostName);
-    writeKey(hostName, key.privateKey);
+    await updateConfig(hostName);
+    await writeKey(hostName, key.privateKey);
     const viewAction = 'View';
     const action = await theia.window.showInformationMessage(`Key pair for ${hostName} successfully generated, do you want to view the public key?`, viewAction);
     if (action === viewAction && key.privateKey) {
@@ -119,9 +119,9 @@ const createKeyPair = async function (sshkeyManager: SshKeyManager): Promise<voi
     await sshkeyManager
         .create({ name: hostName, service: 'vcs', publicKey: publicKey, privateKey })
         .then(async () => {
-            theia.window.showInformationMessage('Key "' + `${hostName}` + '" successfully created');
-            updateConfig(hostName);
-            writeKey(hostName, privateKey);
+            theia.window.showInformationMessage(`Key pair for ${hostName} successfully created`);
+            await updateConfig(hostName);
+            await writeKey(hostName, privateKey);
         })
         .catch(error => {
             theia.window.showErrorMessage(error);
@@ -135,13 +135,13 @@ const deleteKeyPair = async function (sshkeyManager: SshKeyManager): Promise<voi
     const keyName = keyResp ? keyResp.label : '';
     await sshkeyManager
         .delete('vcs', keyName)
-        .then(() => {
-            const keyFile = os.homedir() + '/.ssh/' + keyName.replace('.', '_');
-            if (fs.existsSync(keyFile)) {
-                fs.unlinkSync(keyFile);
-                updateConfig(keyName);
+        .then(async () => {
+            const keyFile = getKeyFilePath(keyName);
+            if (await pathExists(keyFile)) {
+                await unlink(keyFile);
+                await updateConfig(keyName);
             }
-            theia.window.showInformationMessage('Key "' + `${keyName}` + '" successfully deleted');
+            theia.window.showInformationMessage(`Key ${keyName} successfully deleted`);
         })
         .catch(error => {
             theia.window.showErrorMessage(error);
