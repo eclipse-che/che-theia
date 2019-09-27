0fa8c8ce31b81130c6d94bd50d627da099e18ed9
Vitaliy Guliy <vgulyy@redhat.com>
2019-09-27 11:40:22 +0000

    Update Content-Security-Policy
    Signed-off-by: Vitaliy Guliy <vgulyy@redhat.com>

--- plugins/welcome-plugin/src/welcome-plugin.ts
+++ plugins/welcome-plugin/src/welcome-plugin.ts
@@ -37,7 +37,7 @@ async function getHtmlForWebview(context: theia.PluginContext): Promise<string> 
             <html lang="en">
             <head>
                 <meta charset="UTF-8">
-                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; font-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self'; script-src 'unsafe-inline' 'self' ;">
+                <meta http-equiv="Content-Security-Policy" content="font-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'unsafe-inline' 'self';">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link rel="stylesheet" type="text/css" href="${cssUri}">
                 </style>
