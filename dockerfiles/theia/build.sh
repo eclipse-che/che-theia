c732623ecb3be8d2c9b3d3d45a80f411c9d02ef4
nickboldt <nboldt@redhat.com>
2019-10-05 17:00:54 -0400

    support both rhel.Dockerfile and Dockerfile.rhel options (the former lets you edit the file in vscode w/ syntax highlighting support); squash resulting image; echo more container image params to console (not just TAG); add --offline flag (false by default); make organization include the repository explicitly
    Change-Id: I51dcf1a8b10fc26134e7514fc8f0d07cce047f12
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    builder-setup is different by OS; split out runtime-yarninstall and runtime-getplugin steps from runtime-configure step
    
    Change-Id: I12655bf074a1cee14b14eedda45b90a92182bffe
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    rename Dockerfile.rhel -> rhel.Dockerfile (better IDE support for *.Dockerfile than for Dockerfile.*)
    
    Change-Id: Ic9b86ebcf53228d295b2f757559a650f327470a6
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    builder-setup is different by OS; split out runtime-yarninstall and runtime-getplugin steps from runtime-configure step
    
    Change-Id: Iac7a7be4679f19d4159abf5f73dadb01fd34482d
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    builder-setup is different by OS
    
    Change-Id: Ifccde42f88130af6d885c02998269e724b5137c0
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    add support for --offline; skip steps not needed for offline; skip tests if test script not present (eg., in downstream fork)
    
    Change-Id: Ib72fb65b7b025130a5a427bc9445fd003a997ce9
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    add TODO note about externalizing version/repo/branch when fetching sources
    
    Change-Id: I02f95d388ee8499e50c9ec006eed7426e4e98185
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dockerfiles/theia/build.sh
+++ dockerfiles/theia/build.sh
@@ -10,7 +10,9 @@
 base_dir=$(cd "$(dirname "$0")"; pwd)
 . "${base_dir}"/../build.include
 
-check_github_limits
+init --name:theia "$@"
+
+if [[ ${OFFLINE} == "false" ]]; then check_github_limits; fi
 
 DIR=$(cd "$(dirname "$0")"; pwd)
 LOCAL_ASSEMBLY_DIR="${DIR}"/che-theia
@@ -19,35 +21,64 @@ if [ -d "${LOCAL_ASSEMBLY_DIR}" ]; then
   rm -r "${LOCAL_ASSEMBLY_DIR}"
 fi
 
-# In mac os 'cp' cannot create destination dir, so create it first
-mkdir ${LOCAL_ASSEMBLY_DIR}
+if [[ ${OFFLINE} == "false" ]]; then 
+	# In mac os 'cp' cannot create destination dir, so create it first
+	mkdir -p ${LOCAL_ASSEMBLY_DIR}
 
-echo "Compresing 'che-theia' --> ${LOCAL_ASSEMBLY_DIR}/che-theia.tar.gz"
-cd "${DIR}"/../.. && git ls-files -z -c -o --exclude-standard | xargs -0 tar rvf ${LOCAL_ASSEMBLY_DIR}/che-theia.tar.gz
+	echo "Compresing 'che-theia' --> ${LOCAL_ASSEMBLY_DIR}/che-theia.tar.gz"
+	cd "${DIR}"/../.. && git ls-files -z -c -o --exclude-standard | xargs -0 tar rvf ${LOCAL_ASSEMBLY_DIR}/che-theia.tar.gz
 
-init --name:theia "$@"
+	if [ "${CDN_PREFIX:-}" != "" ]; then
+	  BUILD_ARGS+="--build-arg CDN_PREFIX=${CDN_PREFIX} "
+	fi
 
-if [ "${CDN_PREFIX:-}" != "" ]; then
-  BUILD_ARGS+="--build-arg CDN_PREFIX=${CDN_PREFIX} "
+	if [ "${MONACO_CDN_PREFIX:-}" != "" ]; then
+	  BUILD_ARGS+="--build-arg MONACO_CDN_PREFIX=${MONACO_CDN_PREFIX} "
+	fi
 fi
 
-if [ "${MONACO_CDN_PREFIX:-}" != "" ]; then
-  BUILD_ARGS+="--build-arg MONACO_CDN_PREFIX=${MONACO_CDN_PREFIX} "
+# prefetch 3 offline artifacts
+# TODO: also need to collect npm/yarn installed node_modules but can't do that until the container is built
+# TODO: externalize version/repo/branch params so they're not hardcoded here
+if [[ ${OFFLINE} == "true" ]]; then
+	THEIA_VERSION=master
+	THEIA_GITHUB_REPO=eclipse-theia/theia
+	GIT_BRANCH_NAME=v0.11.0 # use tag v0.11.0 or branch master
+	if [[ -d theia-source-code ]]; then rm -fr theia-source-code; fi
+	echo "Check out https://github.com/${THEIA_GITHUB_REPO} from branch ${GIT_BRANCH_NAME} to ./theia-source-code ..."
+	git clone -q --branch ${GIT_BRANCH_NAME} --single-branch --depth 1 https://github.com/${THEIA_GITHUB_REPO} theia-source-code
+
+	CHE_THEIA_GIT_BRANCH_NAME=7.2.0
+	CHE_THEIA_GITHUB_REPO=eclipse/che-theia
+	if [[ -d theia-source-code/che-theia ]]; then rm -fr theia-source-code/che-theia; fi
+	echo "Check out https://github.com/${CHE_THEIA_GITHUB_REPO} from branch ${CHE_THEIA_GIT_BRANCH_NAME} to ./theia-source-code/che-theia ..."
+	git clone -q --branch ${CHE_THEIA_GIT_BRANCH_NAME} --single-branch --depth 1 https://github.com/${CHE_THEIA_GITHUB_REPO} theia-source-code/che-theia
+
+	# create theia-source-code.tgz
+	cd theia-source-code; tar -pczf ../theia-source-code.tgz . && cd ..
+
+	# get default theia plugins needed for the build
+	curl -sSL https://github.com/eclipse/theia-yeoman-plugin/releases/download/untagged-c11870b25a17d20bb7a7/theia_yeoman_plugin.theia -o theia_yeoman_plugin.theia
+	curl -sSL https://github.com/che-incubator/vscode-git/releases/download/1.30.1/vscode-git-1.3.0.1.vsix -o vscode-git-1.3.0.1.vsix
 fi
 
 build
 
-if ! skip_tests; then
+if [ ! skip_tests ] && [[ -x "${base_dir}"/e2e/build.sh ]]; then
   bash "${base_dir}"/e2e/build.sh "$PREFIX-$NAME" "$@"
+else
+  echo "E2E tests skipped."
 fi
 
-echo "Extracting artifacts for the CDN"
-mkdir -p "${base_dir}/theia_artifacts"
-"${base_dir}"/extract-for-cdn.sh "$IMAGE_NAME" "${base_dir}/theia_artifacts"
-LABEL_CONTENT=$(cat "${base_dir}"/theia_artifacts/cdn.json || true 2>/dev/null)
-if [ -n "${LABEL_CONTENT}" ]; then
-  BUILD_ARGS+="--label che-plugin.cdn.artifacts=$(echo ${LABEL_CONTENT} | sed 's/ //g') "
-  echo "Rebuilding with CDN label..."
-  build
-  "${base_dir}"/push-cdn-files-to-akamai.sh
+if [[ -x "${base_dir}"/extract-for-cdn.sh ]]; then
+  echo "Extracting artifacts for the CDN"
+  mkdir -p "${base_dir}/theia_artifacts"
+  "${base_dir}"/extract-for-cdn.sh "$IMAGE_NAME" "${base_dir}/theia_artifacts"
+  LABEL_CONTENT=$(cat "${base_dir}"/theia_artifacts/cdn.json || true 2>/dev/null)
+  if [[ -n "${LABEL_CONTENT}" ]] && [[ -x "${base_dir}"/push-cdn-files-to-akamai.sh ]]; then
+    BUILD_ARGS+="--label che-plugin.cdn.artifacts=$(echo ${LABEL_CONTENT} | sed 's/ //g') "
+    echo "Rebuilding with CDN label..."
+    build
+    "${base_dir}"/push-cdn-files-to-akamai.sh
+  fi
 fi
--- dockerfiles/theia/rhel.Dockerfile
+++ dockerfiles/theia/rhel.Dockerfile
@@ -11,9 +11,10 @@
 ###
 # Builder Image
 #
-FROM ${BUILD_ORGANIZATION}/${BUILD_PREFIX}-theia-dev:${BUILD_TAG} as builder
+# FROM ${BUILD_ORGANIZATION}/${BUILD_PREFIX}-theia-dev:${BUILD_TAG} as builder
+FROM registry-proxy.engineering.redhat.com/rh-osbs/codeready-workspaces-theia-dev-rhel8:2.0 as builder
 
-${INCLUDE:instructions/builder-setup.env}
+${INCLUDE:instructions/${BUILD_IMAGE_TARGET}/builder-setup.env}
 
 # patch electron module by removing native keymap module (no need to have some X11 libraries)
 RUN line_to_delete=$(grep -n native-keymap dev-packages/electron/package.json | cut -d ":" -f 1) && \
@@ -28,12 +29,11 @@ ${INCLUDE:instructions/builder-change-production-permissions.run}
 # Runtime Image
 #
 
-# Use node image
-FROM registry.access.redhat.com/ubi8/nodejs-10 as runtime
+# https://access.redhat.com/containers/?tab=tags#/registry.access.redhat.com/ubi8/nodejs-10
+FROM registry.access.redhat.com/ubi8/nodejs-10:1-51
 
 ${INCLUDE:instructions/runtime-setup.env}
 ${INCLUDE:instructions/runtime-ports.expose}
-
 
 COPY --from=builder /home/theia-dev/theia-source-code/production/plugins /default-theia-plugins
 
@@ -42,11 +42,30 @@ ${INCLUDE:instructions/${BUILD_IMAGE_TARGET}/runtime-install-dependencies.run}
 
 RUN \
     ${INCLUDE:instructions/${BUILD_IMAGE_TARGET}/runtime-adduser.run}
-    ${INCLUDE:instructions/runtime-configure.run}
+    ${INCLUDE:instructions/${BUILD_IMAGE_TARGET}/runtime-yarninstall.run}
 
+${INCLUDE:instructions/${BUILD_IMAGE_TARGET}/runtime-getplugins.run}
 
+${INCLUDE:instructions/runtime-configure.run}
 ${INCLUDE:instructions/runtime-theia-production.copy}
 ${INCLUDE:instructions/runtime-theia.user}
 ${INCLUDE:instructions/runtime-projects.workdir}
-
 ${INCLUDE:instructions/runtime-define.entrypoint}
+
+ENV SUMMARY="Red Hat CodeReady Workspaces - Theia container" \
+    DESCRIPTION="Red Hat CodeReady Workspaces - Theia container" \
+    PRODNAME="codeready-workspaces" \
+    COMPNAME="theia-rhel8" 
+
+LABEL summary="$SUMMARY" \
+      description="$DESCRIPTION" \
+      io.k8s.description="$DESCRIPTION" \
+      io.k8s.display-name="$DESCRIPTION" \
+      io.openshift.tags="$PRODNAME,$COMPNAME" \
+      com.redhat.component="$PRODNAME-$COMPNAME-container" \
+      name="$PRODNAME/$COMPNAME" \
+      version="2.0" \
+      license="EPLv2" \
+      maintainer="Nick Boldt <nboldt@redhat.com>" \
+      io.openshift.expose-services="" \
+      usage=""
