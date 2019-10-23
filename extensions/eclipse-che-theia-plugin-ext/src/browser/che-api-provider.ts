3a71fdafdd79096895992000da714f4fd96d2c74
Igor Vinokur <ivinokur@redhat.com>
2019-10-23 17:18:43 +0300

    CHE-14615 Apply automatic SSH public key upload to Github (#497)
    Signed-off-by: Igor Vinokur <ivinokur@redhat.com>
    
    In case when git clone by ssh url has failed by permission error, ask to upload an SSH public key to GitHub and try again. In case when push by ssh url has failed, ask to upload an SSH public key and show a notification to retry push. Authenticate to GitHub by oAuth if needed.

--- extensions/eclipse-che-theia-plugin-ext/src/browser/che-api-provider.ts
+++ extensions/eclipse-che-theia-plugin-ext/src/browser/che-api-provider.ts
@@ -20,6 +20,7 @@ import { CheSshMainImpl } from './che-ssh-main';
 import { CheDevfileMainImpl } from './che-devfile-main';
 import { CheUserMainImpl } from './che-user-main';
 import { CheProductMainImpl } from './che-product-main';
+import { CheGithubMainImpl } from './che-github-main';
 
 @injectable()
 export class CheApiProvider implements MainPluginApiProvider {
@@ -31,6 +32,7 @@ export class CheApiProvider implements MainPluginApiProvider {
         rpc.set(PLUGIN_RPC_CONTEXT.CHE_VARIABLES_MAIN, new CheVariablesMainImpl(container, rpc));
         rpc.set(PLUGIN_RPC_CONTEXT.CHE_TASK_MAIN, new CheTaskMainImpl(container, rpc));
         rpc.set(PLUGIN_RPC_CONTEXT.CHE_SSH_MAIN, new CheSshMainImpl(container));
+        rpc.set(PLUGIN_RPC_CONTEXT.CHE_GITHUB_MAIN, new CheGithubMainImpl(container));
         rpc.set(PLUGIN_RPC_CONTEXT.CHE_USER_MAIN, new CheUserMainImpl(container));
         rpc.set(PLUGIN_RPC_CONTEXT.CHE_PRODUCT_MAIN, new CheProductMainImpl(container, rpc));
     }
