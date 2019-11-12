28d1c2e1311df44e2327279998defbb86d2e9bb1
Vladyslav Zhukovskyi <vzhukovs@redhat.com>
2019-11-12 10:52:08 +0200

    Add ability to configure borders in quick pick items list on plugin side
    Signed-off-by: Vladyslav Zhukovskyi <vzhukovs@redhat.com>

--- extensions/eclipse-che-theia-terminal/src/browser/contribution/terminal-command-filter.ts
+++ extensions/eclipse-che-theia-terminal/src/browser/contribution/terminal-command-filter.ts
@@ -17,7 +17,10 @@ export const CONTAINER_SOURCE_ATTRIBUTE = 'source';
  * Return list containers with recipe source attribute.
  */
 export function filterRecipeContainers(containers: WorkspaceContainer[]): WorkspaceContainer[] {
-    return containers.filter((container: WorkspaceContainer) =>
-        container.attributes && (!container.attributes[CONTAINER_SOURCE_ATTRIBUTE] || container.attributes[CONTAINER_SOURCE_ATTRIBUTE] === RECIPE_CONTAINER_SOURCE)
-    );
+    return containers.filter(container => isDevContainer(container));
+}
+
+export function isDevContainer(container: WorkspaceContainer): boolean {
+    return container.attributes !== undefined
+        && (!container.attributes[CONTAINER_SOURCE_ATTRIBUTE] || container.attributes[CONTAINER_SOURCE_ATTRIBUTE] === RECIPE_CONTAINER_SOURCE);
 }
