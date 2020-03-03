85beca2408e4e12c85f092a6713748223b153eca
Artem Zatsarynnyi <azatsary@redhat.com>
2020-03-03 19:18:05 +0200

    Update About dialog according to the upstream changes: disable React rendering as we're doing the manipulations directly contentNode
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

--- extensions/eclipse-che-theia-about/src/browser/about-che-theia-dialog.tsx
+++ extensions/eclipse-che-theia-about/src/browser/about-che-theia-dialog.tsx
@@ -9,7 +9,7 @@
  **********************************************************************/
 
 import * as React from 'react';
-import { AboutDialog, AboutDialogProps, ABOUT_EXTENSIONS_CLASS, ABOUT_CONTENT_CLASS } from '@theia/core/lib/browser/about-dialog';
+import { AboutDialog, AboutDialogProps, ABOUT_CONTENT_CLASS } from '@theia/core/lib/browser/about-dialog';
 import { injectable, inject, postConstruct } from 'inversify';
 import { CheProductService, Product } from '@eclipse-che/theia-plugin-ext/lib/common/che-protocol';
 import { ThemeService, Theme } from '@theia/core/lib/browser/theming';
@@ -22,6 +22,8 @@ const jsonDetails = require('../../conf/about-details.json');
 @injectable()
 export class AboutCheTheiaDialog extends AboutDialog {
 
+    protected productInfo: Product;
+
     @inject(CheProductService)
     private productService: CheProductService;
 
@@ -31,122 +33,93 @@ export class AboutCheTheiaDialog extends AboutDialog {
     constructor(
         @inject(AboutDialogProps) protected readonly props: AboutDialogProps
     ) {
-        // use empty header by default
         super({
             title: ''
         });
     }
 
-    /**
-     * Check if theme is dark or not
-     */
-    private isDark(theme: Theme): boolean {
-        const currentThemeId: string = theme.id;
-        return !currentThemeId.includes('light');
-    }
-
-    /**
-     * Returns product header element
-     */
-    protected async getProductHeader(product: Product): Promise<HTMLElement> {
-        const header = document.createElement('div');
-        header.setAttribute('class', 'che-theia-about-product-header');
-
-        const logo = document.createElement('div');
-        logo.setAttribute('class', 'che-theia-about-product-logo');
-        const image = document.createElement('img');
-
-        if (typeof product.logo === 'object') {
-            const productLogo: Logo = product.logo as Logo;
-            if (this.isDark(this.themeService.getCurrentTheme())) {
-                image.setAttribute('src', productLogo.dark);
-            } else {
-                image.setAttribute('src', productLogo.light);
-            }
-
-            // listen on events when the theme is changing to update the logo
-            this.themeService.onThemeChange(e => {
-                if (this.isDark(e.newTheme)) {
-                    image.setAttribute('src', productLogo.dark);
-                } else {
-                    image.setAttribute('src', productLogo.light);
-                }
-            });
-
-        } else {
-            image.setAttribute('src', product.logo);
-        }
-
-        logo.appendChild(image);
-        header.appendChild(logo);
-
-        return header;
-    }
-
     @postConstruct()
     protected async init(): Promise<void> {
-        // Get product info
-        const product = await this.productService.getProduct();
-
-        // Set dialog title
-        this.titleNode.textContent = product.name;
-
-        const messageNode = document.createElement('div');
-        messageNode.classList.add(ABOUT_CONTENT_CLASS);
-        messageNode.appendChild(await this.getProductHeader(product));
-
-        // Che-Theia
-        const cheTheiaTitle = document.createElement('h4');
-
-        const cheTheiaLink = document.createElement('a');
-        cheTheiaLink.setAttribute('href', `https://github.com/eclipse/che-theia/commit/${jsonDetails.cheTheiaSha1}`);
-        cheTheiaLink.setAttribute('target', '_blank');
-        cheTheiaLink.setAttribute('style', 'color: var(--theia-ui-dialog-font-color);');
-        cheTheiaLink.innerHTML = `${jsonDetails.cheTheiaSha1}`;
-
-        const theiaLink = document.createElement('a');
-        theiaLink.setAttribute('href', `https://github.com/eclipse-theia/theia/commit/${jsonDetails.theiaSha1}`);
-        theiaLink.setAttribute('target', '_blank');
-        theiaLink.setAttribute('style', 'color: var(--theia-ui-dialog-font-color);');
-        theiaLink.innerHTML = `${jsonDetails.theiaSha1}`;
-
-        cheTheiaTitle.appendChild(document.createTextNode('Che-Theia@'));
-        cheTheiaTitle.appendChild(cheTheiaLink);
-        cheTheiaTitle.appendChild(document.createTextNode(' using Theia@'));
-        cheTheiaTitle.appendChild(theiaLink);
-        cheTheiaTitle.setAttribute('style', 'margin: 4px; text-align: center; font-family: Roboto, sans-serif; font-weight: 400');
-        messageNode.appendChild(cheTheiaTitle);
-
-        const extensionInfoTitle = document.createElement('h3');
-        extensionInfoTitle.textContent = 'List of extensions:';
-        extensionInfoTitle.setAttribute('style', 'margin-bottom: 4px');
-        messageNode.appendChild(extensionInfoTitle);
-
-        const extensionInfoContent = document.createElement('ul');
-        extensionInfoContent.setAttribute('style', 'height: 120px; margin-left: 10px; margin-top: 0px;');
-        extensionInfoContent.classList.add(ABOUT_EXTENSIONS_CLASS);
-        messageNode.appendChild(extensionInfoContent);
-
-        const extensionsInfos = await this.appServer.getExtensionsInfos();
-
-        extensionsInfos.forEach(extension => {
-            const extensionInfo = document.createElement('li');
-            extensionInfo.textContent = extension.name + ' ' + extension.version;
-            extensionInfoContent.appendChild(extensionInfo);
-        });
-        messageNode.setAttribute('style', 'flex: 1 100%; padding-bottom: calc(var(--theia-ui-padding)*3);');
-        this.contentNode.appendChild(messageNode);
-
-        const date = document.createElement('h4');
-        date.setAttribute('style', 'margin-block-start: 4px; text-align: right; font-weight: 200; font-size: 10px;');
-        date.textContent = `Built ${jsonDetails.date}`;
-        messageNode.appendChild(date);
+        this.productInfo = await this.productService.getProduct();
+        this.extensionsInfos = await this.appServer.getExtensionsInfos();
+        this.update();
+        this.titleNode.textContent = this.productInfo.name;
     }
 
-    // Disable rendering the dialog by React
-    // as we're doing the manipulations directly in contentNode.
     protected render(): React.ReactNode {
-        return <div></div>;
+        return <div className={ABOUT_CONTENT_CLASS}>
+            {this.renderHeader()}
+            {this.renderExtensions()}
+            {this.renderTimestamp()}
+        </div>;
     }
 
+    protected renderHeader(): React.ReactNode {
+        return <div className='che-theia-about-product-header'>
+            {this.getLogo()}
+            {this.getVersion()}
+        </div>;
+    }
+
+    protected getLogo(): React.ReactNode {
+        const productInfo = this.productInfo;
+        let src = '';
+
+        if (typeof productInfo.logo === 'object') {
+            const productLogo: Logo = productInfo.logo as Logo;
+            src = this.isDark(this.themeService.getCurrentTheme()) ? productLogo.dark : productLogo.light;
+
+            this.themeService.onThemeChange(e => {
+                src = this.isDark(e.newTheme) ? productLogo.dark : productLogo.light;
+            });
+        } else {
+            src = productInfo.logo;
+        }
+
+        return <div className='che-theia-about-product-logo'>
+            <img src={src}></img>
+        </div>;
+    }
+
+    private isDark(theme: Theme): boolean {
+        return !theme.id.includes('light');
+    }
+
+    protected getVersion(): React.ReactNode {
+        const style: React.CSSProperties = {
+            margin: '4px',
+            textAlign: 'center',
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: 400
+        };
+        const linkStyle: React.CSSProperties = {
+            color: 'var(--theia-ui-dialog-font-color)'
+        };
+        return <h4 style={style}>
+            Che-Theia@
+            <a
+                href={`https://github.com/eclipse/che-theia/commit/${jsonDetails.cheTheiaSha1}`}
+                target='_blank'
+                style={linkStyle}>
+                {jsonDetails.cheTheiaSha1}
+            </a>
+            {' '}using Theia@
+            <a
+                href={`https://github.com/eclipse-theia/theia/commit/${jsonDetails.theiaSha1}`}
+                target='_blank'
+                style={linkStyle}>
+                {jsonDetails.theiaSha1}
+            </a>
+        </h4>;
+    }
+
+    protected renderTimestamp(): React.ReactNode {
+        const style: React.CSSProperties = {
+            marginBlockStart: '4px',
+            textAlign: 'right',
+            fontWeight: 200,
+            fontSize: '10px'
+        };
+        return <h4 style={style}>Built {jsonDetails.date}</h4>;
+    }
 }
