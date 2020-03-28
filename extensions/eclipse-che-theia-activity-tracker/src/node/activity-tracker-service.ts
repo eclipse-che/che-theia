cae0cc02111c518fd3ade5e66c3df1b58c1d40cd
Artem Zatsarynnyi <azatsary@redhat.com>
2020-03-28 20:56:24 +0200

    Rework activity-tracker extension to respect Che with TLS
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

--- extensions/eclipse-che-theia-activity-tracker/src/node/activity-tracker-service.ts
+++ extensions/eclipse-che-theia-activity-tracker/src/node/activity-tracker-service.ts
@@ -15,10 +15,8 @@
  ********************************************************************************/
 
 import { injectable, inject } from 'inversify';
-import URI from '@theia/core/lib/common/uri';
 import { ActivityTrackerService } from '../common/activity-tracker-protocol';
 import { CheApiService } from '@eclipse-che/theia-plugin-ext/lib/common/che-protocol';
-import * as http from 'http';
 
 /**
  * Server side part of Theia activity tracker.
@@ -30,11 +28,7 @@ import * as http from 'http';
 @injectable()
 export class ActivityTrackerServiceImpl implements ActivityTrackerService {
 
-    private static WORKSPACE_ID_ENV_VAR_NAME = 'CHE_WORKSPACE_ID';
-    private static MACHINE_TOKEN_ENV_VAR_NAME = 'CHE_MACHINE_TOKEN';
-    private static CHE_API_ENV_VAR_NAME = 'CHE_API_INTERNAL';
-
-    // Time before sending next request. If a few requests from frontend(s) are recieved during this period,
+    // Time before sending next request. If a few requests from frontend(s) are received during this period,
     // only one request to workspace master will be sent.
     private static REQUEST_PERIOD_MS = 1 * 60 * 1000;
     // Time before resending request to workspace master if a network error occurs.
@@ -44,48 +38,13 @@ export class ActivityTrackerServiceImpl implements ActivityTrackerService {
 
     // Indicates state of the timer. If true timer is running.
     private isTimerRunning: boolean;
-    // Flag which is used to check if new requests were recieved during timer awaiting.
+    // Flag which is used to check if new requests were received during timer awaiting.
     private isNewRequest: boolean;
-
-    // http or https module to make requestst to Che API.
-    private pinger: { request(option: http.RequestOptions): http.ClientRequest };
-    private activityRequestOptions: http.RequestOptions;
 
     @inject(CheApiService)
     protected cheApiService: CheApiService;
 
     constructor() {
-        const workspaceId = process.env[ActivityTrackerServiceImpl.WORKSPACE_ID_ENV_VAR_NAME];
-        if (!workspaceId) {
-            throw new Error('Cannot retrieve workspace ID');
-        }
-
-        const apiUrlString = process.env[ActivityTrackerServiceImpl.CHE_API_ENV_VAR_NAME];
-        if (!apiUrlString) {
-            throw new Error('Cannot retrieve Che API uri');
-        }
-
-        const apiUrl = new URI(apiUrlString);
-        this.activityRequestOptions = {
-            path: apiUrl.path + '/activity/' + workspaceId,
-            method: 'PUT'
-        };
-        const authorityColonPos = apiUrl.authority.indexOf(':');
-        if (authorityColonPos === -1) {
-            this.activityRequestOptions.hostname = apiUrl.authority;
-            this.activityRequestOptions.port = apiUrl.scheme === 'http' ? 80 : 443;
-        } else {
-            this.activityRequestOptions.hostname = apiUrl.authority.substring(0, authorityColonPos);
-            this.activityRequestOptions.port = apiUrl.authority.substring(authorityColonPos + 1);
-        }
-
-        const token = process.env[ActivityTrackerServiceImpl.MACHINE_TOKEN_ENV_VAR_NAME];
-        if (token) {
-            this.activityRequestOptions.headers = { 'Authorization': 'Bearer ' + token };
-        }
-
-        this.pinger = apiUrl.scheme === 'http' ? require('http') : require('https');
-
         this.isTimerRunning = false;
         this.isNewRequest = false;
     }
@@ -120,15 +79,14 @@ export class ActivityTrackerServiceImpl implements ActivityTrackerService {
 
     private sendRequest(attemptsLeft: number = ActivityTrackerServiceImpl.RETRY_COUNT): void {
         this.cheApiService.submitTelemetryActivity();
-        const request = this.pinger.request(this.activityRequestOptions);
-        request.on('error', (error: Error) => {
+        try {
+            this.cheApiService.updateWorkspaceActivity();
+        } catch (error) {
             if (attemptsLeft > 0) {
                 setTimeout(() => this.sendRequest(), ActivityTrackerServiceImpl.RETRY_REQUEST_PERIOD_MS, --attemptsLeft);
             } else {
                 console.error('Activity tracker: Failed to ping workspace master: ', error.message);
             }
-        });
-        request.end();
+        }
     }
-
 }
