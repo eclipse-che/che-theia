48401f77823a5474f1a6aeef4fa1074f617c97a9
Artem Zatsarynnyi <azatsary@redhat.com>
2020-04-14 17:47:18 +0300

    Set copyright header
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# Copyright (c) 2019 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
 # Release process automation script. 
 # Used to create branch/tag, update the necessary files
 # and trigger release by force pushing changes to the release branch.
