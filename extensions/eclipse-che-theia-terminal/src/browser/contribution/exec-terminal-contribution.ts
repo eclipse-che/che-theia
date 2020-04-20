74bb985c69492c0f7f5962a232b69489d8755475
Roman Nikitenko <rnikiten@redhat.com>
2020-04-20 12:09:32 +0300

    Fix displaying output for vs code tasks
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

--- extensions/eclipse-che-theia-terminal/src/browser/contribution/exec-terminal-contribution.ts
+++ extensions/eclipse-che-theia-terminal/src/browser/contribution/exec-terminal-contribution.ts
@@ -24,6 +24,7 @@ import { filterRecipeContainers } from './terminal-command-filter';
 import { EnvVariablesServer } from '@theia/core/lib/common/env-variables';
 import { isOSX } from '@theia/core/lib/common/os';
 import { TerminalKeybindingContexts } from '@theia/terminal/lib/browser/terminal-keybinding-contexts';
+import { TERMINAL_WIDGET_FACTORY_ID } from '@theia/terminal/lib/browser/terminal-widget-impl';
 
 export const NewTerminalInSpecificContainer = {
     id: 'terminal-in-specific-container:new',
@@ -207,10 +208,16 @@ export class ExecTerminalFrontendContribution extends TerminalFrontendContributi
         let containerName;
         let closeWidgetExitOrError: boolean = true;
 
-        if (options.attributes) {
-            containerName = options.attributes['CHE_MACHINE_NAME'];
+        const attributes = options.attributes;
+        if (attributes) {
+            const isRemoteValue = attributes['remote'];
+            if (isRemoteValue && isRemoteValue.toLowerCase() === 'false') {
+                return super.newTerminal(options);
+            }
 
-            const closeWidgetOnExitOrErrorValue = options.attributes['closeWidgetExitOrError'];
+            containerName = attributes['CHE_MACHINE_NAME'];
+
+            const closeWidgetOnExitOrErrorValue = attributes['closeWidgetExitOrError'];
             if (closeWidgetOnExitOrErrorValue) {
                 closeWidgetExitOrError = closeWidgetOnExitOrErrorValue.toLowerCase() === 'false' ? false : true;
             }
@@ -229,7 +236,10 @@ export class ExecTerminalFrontendContribution extends TerminalFrontendContributi
     }
 
     get all(): TerminalWidget[] {
-        return this.widgetManager.getWidgets(REMOTE_TERMINAL_WIDGET_FACTORY_ID) as TerminalWidget[];
+        const terminalWidgets = this.widgetManager.getWidgets(TERMINAL_WIDGET_FACTORY_ID) as TerminalWidget[];
+        const remoteTerminalWidgets = this.widgetManager.getWidgets(REMOTE_TERMINAL_WIDGET_FACTORY_ID) as TerminalWidget[];
+
+        return [...terminalWidgets, ...remoteTerminalWidgets];
     }
 
     async registerMenus(menus: MenuModelRegistry): Promise<void> {
