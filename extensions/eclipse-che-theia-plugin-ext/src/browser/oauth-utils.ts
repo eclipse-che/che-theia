5e338ab2a64df06d2f773d24ae576f15e5498b55
Igor Vinokur <ivinokur@redhat.com>
2020-04-22 12:44:58 +0300

    fixup! Fix 'is not a function' error

--- extensions/eclipse-che-theia-plugin-ext/src/browser/oauth-utils.ts
+++ extensions/eclipse-che-theia-plugin-ext/src/browser/oauth-utils.ts
@@ -35,11 +35,15 @@ export class OauthUtils {
             }
         });
         window.addEventListener('message', data => {
-            if (this.oAuthPopup && (typeof data.data === 'string') && data.data.startsWith('token:')) {
+            const message = data.data;
+            if (!this.oAuthPopup || typeof message !== 'string') {
+                return;
+            }
+            if (message.startsWith('token:')) {
                 this.oAuthPopup.close();
                 this.userToken = data.data.substring(6, data.data.length);
                 onDidReceiveTokenEmitter.fire(undefined);
-            } else if (data.data.startsWith('status:') && this.oAuthPopup) {
+            } else if (message.startsWith('status:') && this.oAuthPopup) {
                 this.oAuthPopup.postMessage('token:' + (this.machineToken ? this.machineToken : ''), '*');
             }
         });
