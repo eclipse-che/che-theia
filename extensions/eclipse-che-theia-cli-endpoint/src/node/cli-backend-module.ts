07e224140f1250c66721619a02a9e6d407cf52e2
Florent Benoit <fbenoit@redhat.com>
2020-06-16 16:31:55 +0200

    feat(cli-endpoint): Add endpoint allowing to send open files order on all connected clients
    Change-Id: Ibb569efe33a0b3ea651d17840335e06954cd353f
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ extensions/eclipse-che-theia-cli-endpoint/src/node/cli-backend-module.ts
@@ -0,0 +1,24 @@
+/*********************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+
+import { ContainerModule } from 'inversify';
+import { CliEndpoint, cliPath } from '../common';
+import { DefaultCliEndpoint } from './default-cli-endpoint';
+import { ConnectionContainerModule } from '@theia/core/lib/node/messaging/connection-container-module';
+
+const cliServiceModule = ConnectionContainerModule.create(({ bindBackendService }) => {
+    bindBackendService(cliPath, CliEndpoint);
+});
+
+export default new ContainerModule(bind => {
+    bind(DefaultCliEndpoint).toSelf().inSingletonScope();
+    bind(CliEndpoint).toService(DefaultCliEndpoint);
+    bind(ConnectionContainerModule).toConstantValue(cliServiceModule);
+});
