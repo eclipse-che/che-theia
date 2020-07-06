d5ade4668fd9268ddcc7f102573c52623fc29fb0
Artem Zatsarynnyi <azatsary@redhat.com>
2020-07-06 21:56:44 +0300

    Add a delay to wait for explorer to be opened
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

--- dockerfiles/theia/e2e/cypress/integration/theia/typescript.spec.ts
+++ dockerfiles/theia/e2e/cypress/integration/theia/typescript.spec.ts
@@ -69,6 +69,8 @@ context('TypeScript', () => {
             // enable the explorer view
             cy.get('body').type('{ctrl}{cmd}{shift}e')
         }).then(() => {
+            // wait for explorer to be opened
+            cy.wait(2000);
             // select new folder
             cy.get('.p-TabBar-content > #shell-tab-explorer-view-container > div.theia-tab-icon-label > div.p-TabBar-tabIcon.navigator-tab-icon').click({ force: true }).then(() => {
                 cy.get('#files').contains(FOLDER_NAME).click({ force: true });
