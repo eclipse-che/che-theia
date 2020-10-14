095f89603c91d21f994a3fad1934d66d512a6706
Florent Benoit <fbenoit@redhat.com>
2020-10-14 13:21:35 +0200

    fix(cli-endpoint): Make the implementation compliant with interface Interface is about uri, not file
    Change-Id: Ia3f0305ab262619f0d602e810aea14cf6d7bc586
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- extensions/eclipse-che-theia-cli-endpoint/src/node/default-cli-endpoint.ts
+++ extensions/eclipse-che-theia-cli-endpoint/src/node/default-cli-endpoint.ts
@@ -10,7 +10,6 @@
 
 import { inject, injectable, Container } from 'inversify';
 import { CliEndpoint } from '../common';
-import { FileUri } from '@theia/core/lib/node';
 import { CommandService } from '@theia/core/lib/common';
 import { CheMessagingContribution } from '@eclipse-che/theia-messaging/lib/node/messaging/che-messaging-contribution';
 @injectable()
@@ -19,8 +18,7 @@ export class DefaultCliEndpoint implements CliEndpoint {
     @inject(CheMessagingContribution)
     private cheMessagingContribution: CheMessagingContribution;
 
-    async openFile(file: string): Promise<void> {
-        const fileUri = FileUri.create(file);
+    async openFile(uri: string): Promise<void> {
 
         // get all remote connection containers
         const containers = this.cheMessagingContribution.getConnectionContainers();
@@ -31,7 +29,7 @@ export class DefaultCliEndpoint implements CliEndpoint {
         // we do not wait the end of promises
         Promise.all(containers.map(async (connectionContainer: Container) => {
             const commandService: CommandService = connectionContainer.get(CommandService);
-            return commandService.executeCommand('cli-endpoint:open-file', fileUri.toString());
+            return commandService.executeCommand('cli-endpoint:open-file', uri);
         }));
     };
 }
