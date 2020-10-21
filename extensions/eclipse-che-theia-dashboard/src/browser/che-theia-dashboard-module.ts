5ec59f2147dcdddc67facdd7fd775615f854a093
Artem Zatsarynnyi <azatsary@redhat.com>
2020-10-21 17:16:21 +0300

    Remove File-Share command (#884)
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

--- extensions/eclipse-che-theia-dashboard/src/browser/che-theia-dashboard-module.ts
+++ extensions/eclipse-che-theia-dashboard/src/browser/che-theia-dashboard-module.ts
@@ -10,17 +10,10 @@
 
 import { TheiaDashboardClient } from './theia-dashboard-client';
 import { FrontendApplicationContribution } from '@theia/core/lib/browser';
-import { CommandContribution, MenuContribution } from '@theia/core/lib/common';
-import { CheTheiaDashboardFrontendContribution } from './che-theia-dashboard-frontend-contribution';
 
 import { ContainerModule } from 'inversify';
 
 export default new ContainerModule(bind => {
-    // add your contribution bindings here
     bind(TheiaDashboardClient).toSelf().inSingletonScope();
     bind(FrontendApplicationContribution).toDynamicValue(c => c.container.get(TheiaDashboardClient));
-
-    bind(CheTheiaDashboardFrontendContribution).toSelf().inSingletonScope();
-    bind(CommandContribution).toService(CheTheiaDashboardFrontendContribution);
-    bind(MenuContribution).toService(CheTheiaDashboardFrontendContribution);
 });
