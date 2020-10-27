ace5b47399194c8a05d36c6ed61b62c12f48ac07
Artem Zatsarynnyi <azatsary@redhat.com>
2020-10-27 08:40:20 +0200

    Open File Explorer automatically on opening a new Workspace, which doesn't have a previous workbench layout state stored.
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

---
+++ extensions/eclipse-che-theia-workspace/src/browser/explorer-contribution.ts
@@ -0,0 +1,25 @@
+/*********************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+
+import { inject, injectable } from 'inversify';
+import { FrontendApplicationContribution, FrontendApplication, ApplicationShell } from '@theia/core/lib/browser';
+import { FILE_NAVIGATOR_ID } from '@theia/navigator/lib/browser';
+
+@injectable()
+export class ExplorerContribution implements FrontendApplicationContribution {
+
+    @inject(ApplicationShell)
+    protected readonly shell: ApplicationShell;
+
+    // Note, it's called only when there is no previous workbench layout state is stored.
+    async initializeLayout(app: FrontendApplication): Promise<void> {
+        this.shell.revealWidget(FILE_NAVIGATOR_ID);
+    }
+}
