0ead0edba93bb866a6e4f77103908549fc8b40a2
Roman Nikitenko <rnikiten@redhat.com>
2020-11-09 19:58:47 +0200

    Fix formatting issue
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

--- plugins/github-auth-plugin/src/github-auth-plugin.ts
+++ plugins/github-auth-plugin/src/github-auth-plugin.ts
@@ -16,43 +16,43 @@ export async function start(context: theia.PluginContext): Promise<void> {
     const sessions: theia.AuthenticationSession[] = context.workspaceState.get('sessions') || [];
     const onDidChangeSessions = new theia.EventEmitter<theia.AuthenticationProviderAuthenticationSessionsChangeEvent>();
     theia.authentication.registerAuthenticationProvider({
-            id: 'github',
-            label: 'GitHub',
-            supportsMultipleAccounts: false,
-            onDidChangeSessions: onDidChangeSessions.event,
-            getSessions: async () => sessions,
-            login: async (scopes: string[]) => {
-                const githubUser = await che.github.getUser();
-                const session = {
-                    id: v4(),
-                    accessToken: await che.github.getToken(),
-                    account: { label: githubUser.login, id: githubUser.id.toString() },
-                    scopes
-                };
-                const sessionIndex = sessions.findIndex(s => s.id === session.id);
-                if (sessionIndex > -1) {
-                    sessions.splice(sessionIndex, 1, session);
-                } else {
-                    sessions.push(session);
-                }
+        id: 'github',
+        label: 'GitHub',
+        supportsMultipleAccounts: false,
+        onDidChangeSessions: onDidChangeSessions.event,
+        getSessions: async () => sessions,
+        login: async (scopes: string[]) => {
+            const githubUser = await che.github.getUser();
+            const session = {
+                id: v4(),
+                accessToken: await che.github.getToken(),
+                account: { label: githubUser.login, id: githubUser.id.toString() },
+                scopes
+            };
+            const sessionIndex = sessions.findIndex(s => s.id === session.id);
+            if (sessionIndex > -1) {
+                sessions.splice(sessionIndex, 1, session);
+            } else {
+                sessions.push(session);
+            }
+            context.workspaceState.update('sessions', sessions);
+            onDidChangeSessions.fire({ added: [session.id], removed: [], changed: [] });
+            return session;
+        },
+        logout: async (id: string) => {
+            const sessionIndex = sessions.findIndex(session => session.id === id);
+            if (sessionIndex > -1) {
+                sessions.splice(sessionIndex, 1);
                 context.workspaceState.update('sessions', sessions);
-                onDidChangeSessions.fire({ added: [session.id], removed: [], changed: [] });
-                return session;
-            },
-            logout: async (id: string) => {
-                const sessionIndex = sessions.findIndex(session => session.id === id);
-                if (sessionIndex > -1) {
-                    sessions.splice(sessionIndex, 1);
-                    context.workspaceState.update('sessions', sessions);
-                    onDidChangeSessions.fire({ added: [], removed: [id], changed: [] });
-                }
+                onDidChangeSessions.fire({ added: [], removed: [id], changed: [] });
             }
         }
+    }
     );
     if (theia.plugins.getPlugin('github.vscode-pull-request-github')) {
         if (sessions.length > 0) {
             onDidChangeSessions.fire({ added: sessions.map(s => s.id), removed: [], changed: [] });
-        // TODO Remove the notification when https://github.com/eclipse-theia/theia/issues/7178 is fixed.
+            // TODO Remove the notification when https://github.com/eclipse-theia/theia/issues/7178 is fixed.
         } else {
             const signIn = 'Sign in';
             const result = await theia.window.showInformationMessage(
