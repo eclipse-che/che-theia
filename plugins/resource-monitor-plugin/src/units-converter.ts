d216990c6ea7fbb3a36ab5ba2fd95f6e25096461
svor <vsvydenk@redhat.com>
2020-11-11 17:57:53 +0200

    apply formatter
    Signed-off-by: svor <vsvydenk@redhat.com>

--- plugins/resource-monitor-plugin/src/units-converter.ts
+++ plugins/resource-monitor-plugin/src/units-converter.ts
@@ -1,65 +1,65 @@
-/*********************************************************************
- * Copyright (c) 2020 Red Hat, Inc.
+/**********************************************************************
+ * Copyright (c) 2018-2020 Red Hat, Inc.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- **********************************************************************/
+ ***********************************************************************/
 
 export function convertMemory(unit: string | undefined): number {
-    if (!unit) {
-        return 0;
-    }
-    if (unit.substr(unit.length - 2).toUpperCase() === 'MI') {
-        return fromMebibytes(parseInt(unit.substr(0, unit.length - 2)));
-    } else if (unit.substr(unit.length - 2).toUpperCase() === 'KI') {
-        return fromKibibytes(parseInt(unit.substr(0, unit.length - 2)));
-    } else if (unit.substr(unit.length - 2).toUpperCase() === 'GI') {
-        return fromGibibytes(parseInt(unit.substr(0, unit.length - 2)));
-    } else if (unit.substr(unit.length - 1).toUpperCase() === 'M') {
-        return fromMegabytes(parseInt(unit.substr(0, unit.length - 1)));
-    } else if (unit.substr(unit.length - 1).toUpperCase() === 'K') {
-        return fromKilobytes(parseInt(unit.substr(0, unit.length - 1)));
-    } else if (unit.substr(unit.length - 1).toUpperCase() === 'G') {
-        return fromGigabytes(parseInt(unit.substr(0, unit.length - 1)));
-    } else {
-        return parseInt(unit);
-    }
+  if (!unit) {
+    return 0;
+  }
+  if (unit.substr(unit.length - 2).toUpperCase() === 'MI') {
+    return fromMebibytes(parseInt(unit.substr(0, unit.length - 2)));
+  } else if (unit.substr(unit.length - 2).toUpperCase() === 'KI') {
+    return fromKibibytes(parseInt(unit.substr(0, unit.length - 2)));
+  } else if (unit.substr(unit.length - 2).toUpperCase() === 'GI') {
+    return fromGibibytes(parseInt(unit.substr(0, unit.length - 2)));
+  } else if (unit.substr(unit.length - 1).toUpperCase() === 'M') {
+    return fromMegabytes(parseInt(unit.substr(0, unit.length - 1)));
+  } else if (unit.substr(unit.length - 1).toUpperCase() === 'K') {
+    return fromKilobytes(parseInt(unit.substr(0, unit.length - 1)));
+  } else if (unit.substr(unit.length - 1).toUpperCase() === 'G') {
+    return fromGigabytes(parseInt(unit.substr(0, unit.length - 1)));
+  } else {
+    return parseInt(unit);
+  }
 }
 
 export function fromKilobytes(unit: number): number {
-    return unit * 1000;
+  return unit * 1000;
 }
 
 export function fromMegabytes(unit: number): number {
-    return fromKilobytes(unit) * 1000;
+  return fromKilobytes(unit) * 1000;
 }
 
 export function fromGigabytes(unit: number): number {
-    return fromMegabytes(unit) * 1000;
+  return fromMegabytes(unit) * 1000;
 }
 
 export function fromMebibytes(unit: number): number {
-    return fromKibibytes(unit) * 1024;
+  return fromKibibytes(unit) * 1024;
 }
 
 export function fromGibibytes(unit: number): number {
-    return fromMebibytes(unit) * 1024;
+  return fromMebibytes(unit) * 1024;
 }
 
 export function fromKibibytes(unit: number): number {
-    return unit * 1024;
+  return unit * 1024;
 }
 
 export function convertToMilliCPU(unit: string | undefined): number {
-    if (!unit) {
-        return 0;
-    }
-    if (unit.substr(unit.length - 1).toUpperCase() === 'M') {
-        return parseInt(unit.substr(0, unit.length - 1))
-    } else {
-        return parseInt(unit) * 1000;
-    }
+  if (!unit) {
+    return 0;
+  }
+  if (unit.substr(unit.length - 1).toUpperCase() === 'M') {
+    return parseInt(unit.substr(0, unit.length - 1));
+  } else {
+    return parseInt(unit) * 1000;
+  }
 }
