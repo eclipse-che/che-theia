5194a7f4c023ad48a2988f4363fdc7111fbab0fc
Vitaliy Gulyy <vgulyy@redhat.com>
2021-03-04 13:18:14 +0200

    Apply default certificate when downloading ZIP projects
    Signed-off-by: Vitaliy Gulyy <vgulyy@redhat.com>

--- plugins/workspace-plugin/src/ca-cert.ts
+++ plugins/workspace-plugin/src/ca-cert.ts
@@ -16,8 +16,36 @@ const SS_CRT_PATH = '/tmp/che/secret/ca.crt';
 
 const CA_BUNDLE_PATH = '/tmp/ca-bundle.crt';
 
+/**
+ * Possible locations of default system certificates.
+ */
+const systemCerts = [
+  // Debian, Ubuntu, Gentoo
+  '/etc/ssl/certs/ca-certificates.crt',
+  // Fedora, RHEL 6
+  '/etc/pki/tls/certs/ca-bundle.crt',
+  // OpenSUSE
+  '/etc/ssl/ca-bundle.pem',
+  // OpenELEC
+  '/etc/pki/tls/cacert.pem',
+  // CentOS, RHEL 7
+  '/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem',
+  // Alpine
+  '/etc/ssl/cert.pem',
+];
+
 export const getCertificate = new Promise<string | undefined>(async resolve => {
   const certificates: Buffer[] = [];
+
+  // Look for default certificate.
+  // Stop iterating when finding one.
+  for (const cert of systemCerts) {
+    if (await fs.pathExists(cert)) {
+      // read the certificate
+      certificates.push(await fs.readFile(cert));
+      break;
+    }
+  }
 
   if (await fs.pathExists(SS_CRT_PATH)) {
     certificates.push(await fs.readFile(SS_CRT_PATH));
