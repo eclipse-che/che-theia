4370d8dbfdb21601632f6eeb99dc27b550a4c2bf
Vitaliy Gulyy <vgulyy@redhat.com>
2021-03-10 15:58:55 +0200

    List of certificates must include default system certificates
    Signed-off-by: Vitaliy Gulyy <vgulyy@redhat.com>

--- extensions/eclipse-che-theia-plugin-ext/src/node/che-authority-service.ts
+++ extensions/eclipse-che-theia-plugin-ext/src/node/che-authority-service.ts
@@ -17,10 +17,30 @@ import { injectable } from 'inversify';
 export const SS_CRT_PATH = '/tmp/che/secret/ca.crt';
 export const PUBLIC_CRT_PATH = '/public-certs';
 
+/**
+ * Possible locations of default system certificates.
+ */
+const SYSTEM_CERTS = [
+  '/etc/ssl/certs/ca-certificates.crt',
+  '/etc/pki/tls/certs/ca-bundle.crt',
+  '/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem',
+  '/etc/ssl/cert.pem',
+];
+
 @injectable()
 export class CheAuthorityServiceImpl implements CheAuthorityService {
   async getCertificates(): Promise<Buffer[] | undefined> {
     const certificates: Buffer[] = [];
+
+    // Look for default certificate.
+    // Stop iterating when finding one.
+    for (const cert of SYSTEM_CERTS) {
+      if (await fs.pathExists(cert)) {
+        // read the certificate
+        certificates.push(await fs.readFile(cert));
+        break;
+      }
+    }
 
     if (await fs.pathExists(SS_CRT_PATH)) {
       certificates.push(await fs.readFile(SS_CRT_PATH));
