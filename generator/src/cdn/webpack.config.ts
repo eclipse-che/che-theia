e45f32f3e6771b121d54d39854ce8347e282b8c5
Artem Zatsarynnyi <azatsary@redhat.com>
2021-05-20 22:41:35 +0300

    Export a function from the Webpack config to read the passed environment
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

--- generator/src/cdn/webpack.config.ts
+++ generator/src/cdn/webpack.config.ts
@@ -33,7 +33,7 @@ const {
 // Retrieve the default, generated, Theia Webpack configuration
 const baseConfig = require('../webpack.config');
 
-customizeWebpackConfig(cdn, monacopkg, baseConfig);
+// customizeWebpackConfig(cdn, monacopkg, baseConfig);
 
 // Export the customized webpack configuration object
 module.exports = function(env) {
