09578372f481df91eb158a581dccd91e6ecbc969
Artem Zatsarynnyi <azatsary@redhat.com>
2021-05-31 14:52:07 +0300

    Update 'defaultVendors' prop. according to the migration guide
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

--- generator/tests/cdn/webpack-customizer.spec.ts
+++ generator/tests/cdn/webpack-customizer.spec.ts
@@ -89,7 +89,7 @@ describe('Test webpack customizer', () => {
         expect(baseConfig.entry['cdn-support']).toEqual(path.resolve(__dirname, '../../src/cdn/bootstrap.js'));
         expect(baseConfig.output.filename).toBe('[name].[chunkhash].js');
         expect(baseConfig.optimization.runtimeChunk).toBe('single');
-        expect(baseConfig.optimization.splitChunks.cacheGroups.vendors).toEqual({
+        expect(baseConfig.optimization.splitChunks.cacheGroups.defaultVendors).toEqual({
             test: /[\/]node_modules[\/](?!@theia[\/])/,
             name: 'vendors',
             chunks: 'all',
