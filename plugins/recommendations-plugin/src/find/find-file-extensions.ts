58684363af2c0acfd72a9238cf4afcdc7bd9153a
Florent Benoit <fbenoit@redhat.com>
2021-06-14 16:19:36 +0200

    feat(recommendations): delegate find to theia API
    Change-Id: Ie482fbc86c6faca8c74f53ec194a7d3f6672a793
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- plugins/recommendations-plugin/src/find/find-file-extensions.ts
+++ plugins/recommendations-plugin/src/find/find-file-extensions.ts
@@ -7,7 +7,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  ***********************************************************************/
-import * as globby from 'globby';
 import * as theia from '@theia/plugin';
 
 import { injectable } from 'inversify';
@@ -16,48 +15,21 @@ import { injectable } from 'inversify';
 export class FindFileExtensions {
   public static readonly DEFAULT_SCAN_TIME_PER_WORKSPACE_FOLDER: number = 3000;
 
-  async find(
-    workspaceFolders: theia.WorkspaceFolder[],
-    timeout: number = FindFileExtensions.DEFAULT_SCAN_TIME_PER_WORKSPACE_FOLDER
-  ): Promise<string[]> {
-    // get extensions for each theia workspace
-    const extensions: string[][] = await Promise.all(
-      workspaceFolders.map(workspaceFolder => this.findInFolder(workspaceFolder.uri.path, timeout))
-    );
-    return extensions.reduce((acc, e) => acc.concat(e), []);
-  }
+  async find(timeout: number = FindFileExtensions.DEFAULT_SCAN_TIME_PER_WORKSPACE_FOLDER): Promise<string[]> {
+    const cancellationTokenSource = new theia.CancellationTokenSource();
 
-  findInFolder(workspaceFolder: string, timeout: number): Promise<string[]> {
-    // do not let timeout send the event a new time.
-    let alreadyStopped = false;
     setTimeout(() => {
-      if (!alreadyStopped) {
-        stream.emit('end');
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (stream as any).destroy();
-      }
+      cancellationTokenSource.cancel();
     }, timeout);
 
+    const uris = await theia.workspace.findFiles('**/*', undefined, undefined, cancellationTokenSource.token);
     const fileExtensions: string[] = [];
-    const options: globby.GlobbyOptions = {
-      gitignore: true,
-      cwd: workspaceFolder,
-    };
-    const stream = globby.stream('**/*', options);
-    stream.on('data', entry => {
-      const fileExtension = entry.slice(((entry.lastIndexOf('.') - 1) >>> 0) + 1);
+    uris.forEach(uri => {
+      const fileExtension = uri.fsPath.slice(((uri.fsPath.lastIndexOf('.') - 1) >>> 0) + 1);
       if (fileExtension.length > 0 && !fileExtensions.includes(fileExtension)) {
         fileExtensions.push(fileExtension);
       }
     });
-    stream.on('end', () => {
-      alreadyStopped = true;
-    });
-
-    return new Promise<string[]>(resolve => {
-      stream.on('end', () => {
-        resolve(fileExtensions);
-      });
-    });
+    return fileExtensions;
   }
 }
--- plugins/recommendations-plugin/tests/find/find-file-extensions.spec.ts
+++ plugins/recommendations-plugin/tests/find/find-file-extensions.spec.ts
@@ -20,48 +20,63 @@ import { FindFileExtensions } from '../../src/find/find-file-extensions';
 
 describe('Test FindFile implementation', () => {
   let container: Container;
+  let findFilesSpy: any;
+  let cancelMethod: any;
 
   beforeEach(() => {
     jest.restoreAllMocks();
     jest.resetAllMocks();
+    theia.workspace.findFiles = jest.fn();
+    findFilesSpy = jest.spyOn(theia.workspace, 'findFiles');
+    cancelMethod = jest.fn();
+
+    const spy = jest.spyOn(theia.workspace, 'findFiles');
+    (theia as any).CancellationTokenSource = jest.fn().mockImplementation(() => ({
+      cancel: cancelMethod,
+    }));
+
     container = new Container();
     container.bind(FindFileExtensions).toSelf().inSingletonScope();
   });
 
   test('find', async () => {
     const findFileExtensions = container.get(FindFileExtensions);
-    const findPath = path.join(__dirname, '..', '_data', 'find');
-    const uri = { path: findPath };
-    const workspaceFolder = { uri } as theia.WorkspaceFolder;
-    const workspaceFolders: theia.WorkspaceFolder[] = [workspaceFolder];
-    const fileExtensions = await findFileExtensions.find(workspaceFolders);
+    findFilesSpy.mockResolvedValue([{ fsPath: '/tmp/foo.php' } as theia.Uri]);
+    const fileExtensions = await findFileExtensions.find();
     expect(fileExtensions).toBeDefined();
-    expect(fileExtensions.length).toBe(4);
+    expect(fileExtensions.length).toBe(1);
     expect(fileExtensions.includes('.php')).toBeTruthy();
-    expect(fileExtensions.includes('.java')).toBeTruthy();
-    expect(fileExtensions.includes('.py')).toBeTruthy();
-    expect(fileExtensions.includes('.json')).toBeTruthy();
   });
 
-  test('stop fast', async () => {
+  test('stop after', async () => {
     const findFileExtensions = container.get(FindFileExtensions);
-    const findPath = path.join(__dirname, '..', '_data', 'find');
-    const fileExtensions = await findFileExtensions.findInFolder(findPath, 0);
+
+    findFilesSpy.mockImplementation(async () => {
+      await sleep(1000);
+      return [{ fsPath: '/tmp/foo.php' } as theia.Uri, { fsPath: '/tmp/one.php' } as theia.Uri];
+    });
+    const fileExtensions = await findFileExtensions.find(500);
     expect(fileExtensions).toBeDefined();
     expect(fileExtensions.length >= 0).toBeTruthy();
+    // cancel called
+    expect(cancelMethod).toBeCalled();
+  });
+
+  test('stop before', async () => {
+    const findFileExtensions = container.get(FindFileExtensions);
+
+    findFilesSpy.mockImplementation(async () => [
+      { fsPath: '/tmp/foo.php' } as theia.Uri,
+      { fsPath: '/tmp/one.php' } as theia.Uri,
+    ]);
+    const fileExtensions = await findFileExtensions.find(100);
+    expect(fileExtensions).toBeDefined();
+    expect(fileExtensions.length >= 0).toBeTruthy();
+    // cancel called not called
+    expect(cancelMethod).toBeCalledTimes(0);
   });
 
   function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-
-  test('multiple end event', async () => {
-    (globby as any).__setStreamEnd();
-    const findFileExtensions = container.get(FindFileExtensions);
-    const findPath = path.join(__dirname, '..', '_data', 'find');
-    const fileExtensions = await findFileExtensions.findInFolder(findPath, 100);
-    await sleep(1000);
-    expect(fileExtensions).toBeDefined();
-    expect(fileExtensions.length).toBe(4);
-  });
 });
