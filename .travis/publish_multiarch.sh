a886e8f3bb6a4251bbc495cd3e8c3b7cac4d0ef4
vibhutisawant <Vibhuti.Sawant@ibm.com>
2021-06-23 20:14:59 +0530

    Adds travis suffix and removes workflows handled on GA

--- .travis/publish_multiarch.sh
+++ .travis/publish_multiarch.sh
@@ -15,17 +15,22 @@ SHORT_SHA=$(git rev-parse --short HEAD)
 for image in "${PUBLISH_IMAGES_LIST[@]}"
   do
     AMEND=""
-    AMEND+=" --amend ${REGISTRY}/${image}:${TAG}-amd64";
-    AMEND+=" --amend ${REGISTRY}/${image}:${TAG}-arm64";
-    AMEND+=" --amend ${REGISTRY}/${image}:${TAG}-ppc64le";
-    AMEND+=" --amend ${REGISTRY}/${image}:${TAG}-s390x";
+    AMEND+=" --amend ${REGISTRY}/${image}:${TAG}-${TRAVIS_TAG}-amd64";
+    AMEND+=" --amend ${REGISTRY}/${image}:${TAG}-${TRAVIS_TAG}-arm64";
+    AMEND+=" --amend ${REGISTRY}/${image}:${TAG}-${TRAVIS_TAG}-ppc64le";
+    AMEND+=" --amend ${REGISTRY}/${image}:${TAG}-${TRAVIS_TAG}-s390x";
 
-    docker manifest create ${REGISTRY}/${image}:"${TAG}" $AMEND
-    docker manifest push ${REGISTRY}/${image}:"${TAG}"
+    docker manifest create ${REGISTRY}/${image}:"${TAG}-${TRAVIS_TAG}" $AMEND
+    docker manifest push ${REGISTRY}/${image}:"${TAG}-${TRAVIS_TAG}"
+
+    if [[ "${TAG}" != "next" ]]; then
+       docker manifest create ${REGISTRY}/${image}:"latest-${TRAVIS_TAG}" $AMEND
+       docker manifest push ${REGISTRY}/${image}:"latest-${TRAVIS_TAG}"
+    fi
 
     if [[ "${TAG}" == "next" ]]; then
-       docker manifest create ${REGISTRY}/${image}:"${SHORT_SHA}" $AMEND
-       docker manifest push ${REGISTRY}/${image}:"${SHORT_SHA}"
+       docker manifest create ${REGISTRY}/${image}:"${SHORT_SHA}-${TRAVIS_TAG}" $AMEND
+       docker manifest push ${REGISTRY}/${image}:"${SHORT_SHA}-${TRAVIS_TAG}"
     fi
 
   done
