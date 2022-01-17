7bb78c2e82ddb23fe2918d450807a7c8709d79d7
Igor Vinokur <ivinokur@redhat.com>
2022-01-17 12:06:25 +0200

    chore: Rework GitHub authentication flow
    Signed-off-by: Igor Vinokur <ivinokur@redhat.com>

---
+++ extensions/eclipse-che-theia-plugin-ext/src/node/che-github-service.ts
@@ -0,0 +1,26 @@
+/**********************************************************************
+ * Copyright (c) 2018-2022 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ***********************************************************************/
+import * as fs from 'fs-extra';
+import * as os from 'os';
+import * as path from 'path';
+
+import { CheGitHubService } from '../common/che-protocol';
+import { injectable } from 'inversify';
+
+@injectable()
+export class CheGithubServiceImpl implements CheGitHubService {
+  async getToken(): Promise<string | undefined> {
+    const credentialsPath = path.resolve(os.homedir(), '.git-credentials', 'credentials');
+    if (fs.existsSync(credentialsPath)) {
+      const token = fs.readFileSync(credentialsPath).toString();
+      return token.substring(token.lastIndexOf(':') + 1, token.indexOf('@'));
+    }
+  }
+}
